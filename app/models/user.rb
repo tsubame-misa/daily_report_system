2b598fe4e978a20b24e8f5bec599a5e0640baf37
tsubame-misa <watamisa080513@gmail.com>
2025-04-23 15:24:33 +0900

    ユーザーモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :reports, dependent: :destroy
+
+  # role が 0 なら管理者
+  def admin?
+    role == 0
+  end
 end
