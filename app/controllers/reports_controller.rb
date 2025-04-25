0c1cbe0e3044c09905d3196b4978701dfd423150
okozeriki <riki21061216@gmail.com>
2025-04-25 13:39:22 +0900

    不必要なコメントを削除し、ユーザー表示ボタンを追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,5 @@
 class ReportsController < ApplicationController
   def index
-    # current_user.reports.find(params[:id]
     @reports = Report.where(user_id: current_user.id).order(created_at: :desc)
     # admin.reports.indexを表示
     render 'admin/reports/index'
