7cc4a4f3c71fb336655bd6d9074acfd4384a1763
So.I <main@sokz.net>
2025-04-25 15:23:11 +0900

    #14 wip 編集ページ・削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,24 +1,46 @@
 class ReportsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_report, only: [:edit, :update, :destroy]
 
+  # def index
+  # end
   def new
     @report = current_user.reports.build
   end
 
   def create
-
     @report = current_user.reports.build(report_params)
     if @report.save
       redirect_to reports_path, notice: "日報が作成されました。"
-      
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  private
-  def report_params
-    params.require(:report).permit(:title, :contents)
+  def edit
   end
 
+  def update
+    @report.update(report_params)
+    if @report.save
+      redirect_to reports_path, notice: "日報を更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @report.destroy
+    redirect_to reports_path, notice: "日報が削除されました。"
+  end
+
+  private
+
+  def set_report
+    @report = current_user.reports.find(params[:id])
+  end
+
+  def report_params
+    params.require(:report).permit(:title, :contents, :report_date)
+  end
 end
