adfcc7fcde2cd70da9e87ec340018bd3534cbda6
Keisuke05410 <keisuke3469@gmail.com>
2025-04-25 10:46:08 +0900

    #16 一般ユーザーの追加とそれに紐づくreportの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,22 @@ Report.find_or_create_by!(title: 'admin_title2', contents: 'admin_contents2') do
   report.title = 'admin_title2'
   report.contents = 'admin_contents2'
 end
+
+member = User.find_or_create_by!(email: 'member@example.com') do |user|
+  user.name = 'Member'
+  user.password = 'password'
+  user.password_confirmation = 'password'
+  user.role = 1
+end
+
+Report.find_or_create_by!(title: 'member_title1', contents: 'member_contents1') do |report|
+  report.user_id = member.id
+  report.title = 'member_title1'
+  report.contents = 'member_contents1'
+end
+
+Report.find_or_create_by!(title: 'member_title2', contents: 'member_contents2') do |report|
+  report.user_id = member.id
+  report.title = 'member_title2'
+  report.contents = 'member_contents2'
+end
