22dc3d3421698f03ef5fafc7f86accf6ee9e8827
So.I <main@sokz.net>
2025-04-25 17:36:19 +0900

    #14 wip 編集ページ

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,7 @@
 class ReportsController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :set_report, only: [:edit, :update, :destroy]
+
   def index
     @reports = Report.where(user_id: current_user.id).order(created_at: :desc)
   end
@@ -8,7 +9,7 @@ class ReportsController < ApplicationController
   def show
     @report = Report.find_by(id: params[:id], user_id: current_user.id)
   end
-  
+
   def new
     @report = current_user.reports.build
   end
@@ -27,4 +28,30 @@ class ReportsController < ApplicationController
     params.require(:report).permit(:title, :contents, :report_date)
   end
 
+  def edit
+  end
+
+  def update
+    @report.update(report_params)
+    if @report.save
+      redirect_to reports_path, notice: "日報を更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @report.destroy
+    redirect_to reports_path, notice: "日報が削除されました。"
+  end
+
+  private
+
+  def set_report
+    @report = current_user.reports.find(params[:id])
+  end
+
+  def report_params
+    params.require(:report).permit(:title, :contents, :report_date)
+  end
 end
