e6657d93dbf6f4130c6c7486812a75678ba05427
Keisuke05410 <keisuke3469@gmail.com>
2025-04-25 11:07:07 +0900

    #16 変数名を微調整 admin -> admin1 member -> member1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,37 @@
-admin = User.find_or_create_by!(email: 'admin@example.com') do |user|
-  user.name = 'Admin'
+admin1 = User.find_or_create_by!(email: 'admin1@example.com') do |user|
+  user.name = 'Admin1'
   user.password = 'password'
   user.password_confirmation = 'password'
   user.role = 0
 end
 
-Report.find_or_create_by!(title: 'admin_title1', contents: 'admin_contents1') do |report|
-  report.user_id = admin.id
-  report.title = 'admin_title1'
-  report.contents = 'admin_contents1'
+Report.find_or_create_by!(title: 'admin1_title1', contents: 'admin1_contents1') do |report|
+  report.user_id = admin1.id
+  report.title = 'admin1_title1'
+  report.contents = 'admin1_contents1'
 end
 
-Report.find_or_create_by!(title: 'admin_title2', contents: 'admin_contents2') do |report|
-  report.user_id = admin.id
-  report.title = 'admin_title2'
-  report.contents = 'admin_contents2'
+Report.find_or_create_by!(title: 'admin1_title2', contents: 'admin1_contents2') do |report|
+  report.user_id = admin1.id
+  report.title = 'admin1_title2'
+  report.contents = 'admin1_contents2'
 end
 
-member = User.find_or_create_by!(email: 'member@example.com') do |user|
-  user.name = 'Member'
+member1 = User.find_or_create_by!(email: 'member1@example.com') do |user|
+  user.name = 'Member1'
   user.password = 'password'
   user.password_confirmation = 'password'
   user.role = 1
 end
 
-Report.find_or_create_by!(title: 'member_title1', contents: 'member_contents1') do |report|
-  report.user_id = member.id
-  report.title = 'member_title1'
-  report.contents = 'member_contents1'
+Report.find_or_create_by!(title: 'member1_title1', contents: 'member1_contents1') do |report|
+  report.user_id = member1.id
+  report.title = 'member1_title1'
+  report.contents = 'member1_contents1'
 end
 
-Report.find_or_create_by!(title: 'member_title2', contents: 'member_contents2') do |report|
-  report.user_id = member.id
-  report.title = 'member_title2'
-  report.contents = 'member_contents2'
+Report.find_or_create_by!(title: 'member1_title2', contents: 'member1_contents2') do |report|
+  report.user_id = member1.id
+  report.title = 'member1_title2'
+  report.contents = 'member1_contents2'
 end
