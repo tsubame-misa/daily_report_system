2cdb5ea915bb8849ef144f251ee062fb6cb7bacd
okozeriki <riki21061216@gmail.com>
2025-04-28 12:00:16 +0900

    userのホーム画面でreportsにリダイレクトするように変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @reports = Report.order(created_at: :desc).limit(3) || []
+    # adminじゃなかったら、reportsにリダイレクトする
+    if user_signed_in? && !current_user.admin?
+      redirect_to reports_path
+    else
+      @reports = Report.order(created_at: :desc).limit(3) || []
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "reports#index"
+  root "home#index"
   
   resources :users, only: [:index, :show, :edit, :update, :destroy]
   resources :reports
