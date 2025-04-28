7a28157e1f119ff91982a8f1c42cbe0c50f4644e
okozeriki <riki21061216@gmail.com>
2025-04-28 16:27:54 +0900

    conflictを修正

--- app/models/report.rb
+++ app/models/report.rb
@@ -7,13 +7,14 @@ class Report < ApplicationRecord
     order(column => direction)
   }
 
-  # validates :date, presence: true
+  scope :by_date_range, ->(start_date, end_date) {
+    where(report_date: start_date..end_date) if start_date.present? && end_date.present?
+  }
+
   validates :title, presence: true
   validates :contents, presence: true
   validates :report_date, uniqueness: { 
     scope: :user_id,
     message: "同じ日付の日報はすでに存在します。"
   }, presence: true
-
-    
 end
