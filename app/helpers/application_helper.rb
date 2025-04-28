95b0a28db0ac71940200a20c247f0040f3490218
E-taku <tkymtk8270@gmail.com>
2025-04-28 15:20:25 +0900

    #58 add 管理画面の日報一覧でソート方向を切り替える矢印リンクを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,12 @@ module ApplicationHelper
     if params[:sort] == column
       direction = params[:direction] == "asc" ? "desc" : "asc"
       arrow = params[:direction] == "asc" ? "↑" : "↓"
-      link_to arrow, admin_reports_path(sort: column, direction: direction), class: "btn btn-sm btn-outline-secondary"
     else
-      link_to "↓", admin_reports_path(sort: column, direction: "desc"), class: "btn btn-sm btn-outline-secondary"
+      direction = "asc"
+      arrow = "↓"
     end
+
+    link_to arrow, admin_reports_path(sort: column, direction: direction), class: "btn btn-sm btn-outline-secondary"
   end
 end
 
