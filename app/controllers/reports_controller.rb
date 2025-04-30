985e78e7e5af127d2cba58b9de1c5204e8d6cc46
Keisuke05410 <keisuke3469@gmail.com>
2025-04-30 10:58:46 +0900

    #59 editページでもnot_foundページに飛ぶように修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_report, only: [:edit, :update, :destroy]
+  before_action :set_report, only: %i[show edit update destroy]
 
   def index
     start_date = params[:start_date]
@@ -10,15 +10,12 @@ class ReportsController < ApplicationController
       @reports = Report.where(user_id: current_user.id).order(created_at: :desc)
     else
       @reports = Report.where(user_id: current_user.id)
-                     .by_date_range(start_date, end_date)
-                     .order(created_at: :desc)
+                       .by_date_range(start_date, end_date)
+                       .order(created_at: :desc)
     end
   end
 
-  def show
-    @report = Report.find_by(id: params[:id], user_id: current_user.id)
-    not_found unless @report
-  end
+  def show; end
 
   def new
     @report = current_user.reports.build
@@ -27,7 +24,7 @@ class ReportsController < ApplicationController
   def create
     @report = current_user.reports.build(report_params)
     if @report.save
-      redirect_to reports_path, notice: "日報が作成されました。"
+      redirect_to reports_path, notice: '日報が作成されました。'
     else
       render :new, status: :unprocessable_entity
     end
@@ -36,7 +33,7 @@ class ReportsController < ApplicationController
   def update
     @report.update(report_params)
     if @report.save
-      redirect_to reports_path, notice: "日報を更新しました。"
+      redirect_to reports_path, notice: '日報を更新しました。'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -44,13 +41,14 @@ class ReportsController < ApplicationController
 
   def destroy
     @report.destroy
-    redirect_to reports_path, notice: "日報が削除されました。"
+    redirect_to reports_path, notice: '日報が削除されました。'
   end
 
   private
 
   def set_report
-    @report = current_user.reports.find(params[:id])
+    @report = current_user.reports.find_by(id: params[:id])
+    not_found unless @report
   end
 
   def report_params
