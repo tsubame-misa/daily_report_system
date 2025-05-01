1d5dc24c4d5c1d700888d4f76340503e0eeabfa9
Keisuke05410 <keisuke3469@gmail.com>
2025-05-01 10:07:01 +0900

    query parameterを正しく取得

--- app/controllers/admin/calendar_controller.rb
+++ app/controllers/admin/calendar_controller.rb
@@ -8,28 +8,28 @@ class Admin::CalendarController < ApplicationController
     @favorite_only = ActiveModel::Type::Boolean.new.cast(params[:favorite_only])
     selected_date = params[:date].present? ? Date.parse(params[:date]) : Date.today
     @selected_date = selected_date
-  
+
     @reports = Report.includes(:user)
     @reports = @reports.where(report_date: selected_date)
-  
+
     if start_date.present? && end_date.present? && start_date > end_date
       @date_range_error = '開始日が終了日より後になっています。正しい日付範囲を指定してください。'
     else
       @reports = @reports.by_date_range(start_date, end_date)
     end
-  
+
     @reports = @reports
-                 .sorted_by(sort, direction)
-                 .keyword_search(keyword)
-    if @favorite_only
-      favorite_report_ids = current_user.favorites.pluck(:report_id)
-      @reports = @reports.where(id: favorite_report_ids)
-    end
+               .sorted_by(sort, direction)
+               .keyword_search(keyword)
+    return unless @favorite_only
+
+    favorite_report_ids = current_user.favorites.pluck(:report_id)
+    @reports = @reports.where(id: favorite_report_ids)
   end
+
   def month
-    params[:direction]
-    @favorite_only = ActiveModel::Type::Boolean.new.cast(params[:favorite_only])
-    @selected_date = params[:date].present? ? Date.parse(params[:date]) : Date.today
+    # monthというqueryパラメータがあれば、その月を表示
+    @selected_date = params[:month].present? ? Date.parse("#{params[:month]}-01") : Date.today
     @selected_month = @selected_date.month
     @selected_year = @selected_date.year
 
