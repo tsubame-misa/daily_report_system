1f12b900f7e7b445accfef1f74dc71da65ca5483
So.I <main@sokz.net>
2025-05-02 11:37:52 +0900

    #147 formで表示される日付を決定する

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,13 @@
 module ReportsHelper
+  # _formで表示される日報の日付を決定する
+  # 優先順位:
+  # 1. report.report_date
+  # 2. URLパラメータのdate
+  # 3. 今日の日付
+  def determine_report_date(report)
+    return report.report_date if report.report_date.present?
+    return Date.parse(params[:date]) if params[:date].present?
+
+    Date.today
+  end
 end
