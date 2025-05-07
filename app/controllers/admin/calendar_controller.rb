c6e16e30fce7dc15d892d149060c19764488698b
Keisuke05410 <keisuke3469@gmail.com>
2025-05-07 12:04:24 +0900

    #154 日別のページのヘッダー完成

--- app/controllers/admin/calendar_controller.rb
+++ app/controllers/admin/calendar_controller.rb
@@ -1,31 +1,27 @@
 class Admin::CalendarController < Admin::BaseController
   before_action :authenticate_user!
   def day
-    start_date = params[:start_date]
-    end_date   = params[:end_date]
-    keyword    = params[:q]
-    sort       = params[:sort]
-    direction  = params[:direction]
-    @favorite_only = ActiveModel::Type::Boolean.new.cast(params[:favorite_only])
-    selected_date = params[:date].present? ? Date.parse(params[:date]) : Date.today
-    @selected_date = selected_date
+    @selected_date = params[:day].present? ? Date.parse("#{params[:day]}") : Date.today
+    @previous_date = (@selected_date - 1.day).strftime('%Y-%m-%d')
+    @next_date = (@selected_date + 1.day).strftime('%Y-%m-%d')
+
+    # start_date = params[:start_date]
+    # end_date   = params[:end_date]
+    # keyword    = params[:q]
+    # sort       = params[:sort]
+    # direction  = params[:direction]
+    # @favorite_only = ActiveModel::Type::Boolean.new.cast(params[:favorite_only])
 
     @reports = Report.includes(:user)
-    @reports = @reports.where(report_date: selected_date)
+    @reports = @reports.where(report_date: @selected_date)
 
-    if start_date.present? && end_date.present? && start_date > end_date
-      @date_range_error = '開始日が終了日より後になっています。正しい日付範囲を指定してください。'
-    else
-      @reports = @reports.by_date_range(start_date, end_date)
-    end
+    # @reports = @reports
+    #            .sorted_by(sort, direction)
+    #            .keyword_search(keyword)
+    # return unless @favorite_only
 
-    @reports = @reports
-               .sorted_by(sort, direction)
-               .keyword_search(keyword)
-    return unless @favorite_only
-
-    favorite_report_ids = current_user.favorites.pluck(:report_id)
-    @reports = @reports.where(id: favorite_report_ids)
+    # favorite_report_ids = current_user.favorites.pluck(:report_id)
+    # @reports = @reports.where(id: favorite_report_ids)
   end
 
   def month
