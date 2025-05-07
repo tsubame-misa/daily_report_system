b4b0d483e795bd1d4db4ead5e5dd42e57020cb4a
So.I <main@sokz.net>
2025-05-07 11:22:05 +0900

    #157 一般のリスト表示ヘッダーのUI調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,37 @@ module ApplicationHelper
   def sort_arrow(column)
     if params[:sort] == column
       direction = params[:direction] == "asc" ? "desc" : "asc"
-      arrow = params[:direction] == "asc" ? "↑" : "↓"
+      arrow = params[:direction] == "asc" ? "▲" : "▼"
     else
       direction = "asc"
-      arrow = "↓"
+      arrow = "▼"
     end
     base_params = request.query_parameters.merge(sort: column, direction: direction)
-    link_to arrow, url_for(base_params), class: "btn btn-sm btn-outline-secondary"
+    link_to arrow, url_for(base_params), class: "btn btn-sm arrow-button"
   end
+
+  def favorite_filter_link
+    base_params = request.query_parameters
+    is_favorite = params[:favorite_only] == "true"
+
+    if is_favorite
+      base_params = base_params.except(:favorite_only)
+    else
+      base_params = base_params.merge(favorite_only: "true")
+    end
+
+    link_to url_for(base_params), class: "d-flex align-items-center text-decoration-none" do
+      content_tag(:div, class: "d-flex align-items-center gap-2") do
+        content_tag(:div, class: "form-check form-switch") do
+          content_tag(:input, "", type: "checkbox", class: "form-check-input form-switch-lg", checked: is_favorite) +
+            content_tag(:div, class: "d-flex align-items-center") do
+              content_tag(:span, "お気に入りのみ表示", class: "text-dark")
+            end
+        end
+      end
+    end
+  end
+
   def render_filter(path:)
     render partial: 'layouts/filter', locals: { filter_path: path }
   end
